1d25fc398cf072d85957cf6106dfa7d2184cfdf8
Denis <spiridonovdenis83@gmail.com>
2020-02-11 08:15:39 +0000

    Access token removed

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -9,9 +9,7 @@ export class ConfigService {
   constructor( private httpClient: HttpClient ) { }
 
   getData(location: string, sinceDate: Date, untilDate: Date) {
-    const headerParams = new HttpHeaders().set('Authorization', 'token 596bb52edd15a4a1c64aa08123b5d1a242455c39')
     return this.httpClient.get(`https://api.github.com/repos/${location}/commits`, {
-      headers: headerParams,
       params: {
         "since": `${sinceDate.toISOString()}`,
         "until": `${untilDate.toISOString()}`,
